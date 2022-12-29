73f29d8aa99b924eb6613da11b0dba6af347fd56
tsold <tsoldan.zg@gmail.com>
2022-12-29 16:16:52 +0100

    Added Client and Clinic controllers

---
+++ MedicoHelpAPi/Controllers/ClientsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedicoHelpAPi.Models;
+
+namespace MedicoHelpAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly MedicoContext _context;
+
+        public ClientsController(MedicoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClient()
+        {
+            return await _context.Client.ToListAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(Guid id)
+        {
+            var client = await _context.Client.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        // PUT: api/Clients/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient(Guid id, Client client)
+        {
+            if (id != client.Idclient)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clients
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            _context.Client.Add(client);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClientExists(client.Idclient))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetClient", new { id = client.Idclient }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Client>> DeleteClient(Guid id)
+        {
+            var client = await _context.Client.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Client.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return client;
+        }
+
+        private bool ClientExists(Guid id)
+        {
+            return _context.Client.Any(e => e.Idclient == id);
+        }
+    }
+}
---
+++ MedicoHelpAPi/Controllers/ClinicsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedicoHelpAPi.Models;
+
+namespace MedicoHelpAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinicsController : ControllerBase
+    {
+        private readonly MedicoContext _context;
+
+        public ClinicsController(MedicoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Clinics
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Clinic>>> GetClinic()
+        {
+            return await _context.Clinic.ToListAsync();
+        }
+
+        // GET: api/Clinics/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Clinic>> GetClinic(Guid id)
+        {
+            var clinic = await _context.Clinic.FindAsync(id);
+
+            if (clinic == null)
+            {
+                return NotFound();
+            }
+
+            return clinic;
+        }
+
+        // PUT: api/Clinics/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClinic(Guid id, Clinic clinic)
+        {
+            if (id != clinic.Idclinic)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(clinic).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClinicExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clinics
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Clinic>> PostClinic(Clinic clinic)
+        {
+            _context.Clinic.Add(clinic);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClinicExists(clinic.Idclinic))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetClinic", new { id = clinic.Idclinic }, clinic);
+        }
+
+        // DELETE: api/Clinics/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Clinic>> DeleteClinic(Guid id)
+        {
+            var clinic = await _context.Clinic.FindAsync(id);
+            if (clinic == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clinic.Remove(clinic);
+            await _context.SaveChangesAsync();
+
+            return clinic;
+        }
+
+        private bool ClinicExists(Guid id)
+        {
+            return _context.Clinic.Any(e => e.Idclinic == id);
+        }
+    }
+}
