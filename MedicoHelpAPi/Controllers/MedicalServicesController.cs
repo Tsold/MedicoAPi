712ccd7219110e490ad629edefc6e6d99e876d88
tsold <tsoldan.zg@gmail.com>
2023-01-17 16:30:38 +0100

    Added category,subcategory and medicalservice controllers

---
+++ MedicoHelpAPi/Controllers/MedicalServicesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedicoHelpAPi.Models;
+
+namespace MedicoHelpAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicalServicesController : ControllerBase
+    {
+        private readonly MedicoContext _context;
+
+        public MedicalServicesController(MedicoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MedicalServices
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MedicalService>>> GetMedicalService()
+        {
+            return await _context.MedicalService.ToListAsync();
+        }
+
+        // GET: api/MedicalServices/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MedicalService>> GetMedicalService(Guid id)
+        {
+            var medicalService = await _context.MedicalService.FindAsync(id);
+
+            if (medicalService == null)
+            {
+                return NotFound();
+            }
+
+            return medicalService;
+        }
+
+        // PUT: api/MedicalServices/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedicalService(Guid id, MedicalService medicalService)
+        {
+            if (id != medicalService.Idservice)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(medicalService).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicalServiceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MedicalServices
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<MedicalService>> PostMedicalService(MedicalService medicalService)
+        {
+            _context.MedicalService.Add(medicalService);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MedicalServiceExists(medicalService.Idservice))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetMedicalService", new { id = medicalService.Idservice }, medicalService);
+        }
+
+        // DELETE: api/MedicalServices/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MedicalService>> DeleteMedicalService(Guid id)
+        {
+            var medicalService = await _context.MedicalService.FindAsync(id);
+            if (medicalService == null)
+            {
+                return NotFound();
+            }
+
+            _context.MedicalService.Remove(medicalService);
+            await _context.SaveChangesAsync();
+
+            return medicalService;
+        }
+
+        private bool MedicalServiceExists(Guid id)
+        {
+            return _context.MedicalService.Any(e => e.Idservice == id);
+        }
+    }
+}
---
+++ MedicoHelpAPi/Controllers/SubcategoriesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MedicoHelpAPi.Models;
+
+namespace MedicoHelpAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubcategoriesController : ControllerBase
+    {
+        private readonly MedicoContext _context;
+
+        public SubcategoriesController(MedicoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Subcategories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Subcategory>>> GetSubcategory()
+        {
+            return await _context.Subcategory.ToListAsync();
+        }
+
+        // GET: api/Subcategories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Subcategory>> GetSubcategory(Guid id)
+        {
+            var subcategory = await _context.Subcategory.FindAsync(id);
+
+            if (subcategory == null)
+            {
+                return NotFound();
+            }
+
+            return subcategory;
+        }
+
+        // PUT: api/Subcategories/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubcategory(Guid id, Subcategory subcategory)
+        {
+            if (id != subcategory.Idsubcategory)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subcategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubcategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Subcategories
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Subcategory>> PostSubcategory(Subcategory subcategory)
+        {
+            _context.Subcategory.Add(subcategory);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SubcategoryExists(subcategory.Idsubcategory))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSubcategory", new { id = subcategory.Idsubcategory }, subcategory);
+        }
+
+        // DELETE: api/Subcategories/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Subcategory>> DeleteSubcategory(Guid id)
+        {
+            var subcategory = await _context.Subcategory.FindAsync(id);
+            if (subcategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.Subcategory.Remove(subcategory);
+            await _context.SaveChangesAsync();
+
+            return subcategory;
+        }
+
+        private bool SubcategoryExists(Guid id)
+        {
+            return _context.Subcategory.Any(e => e.Idsubcategory == id);
+        }
+    }
+}
